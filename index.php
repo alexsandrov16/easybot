37db905d83226792b90d1114a7b790d82bf1b62f
alexsandrov16 <al3x5.dev@gmail.com>
2024-05-14 23:16:24 -0400

    Ready for send messages

--- index.php
+++ index.php
@@ -20,33 +20,37 @@ $request = new Request;
 $json = $request->jsonData();
 //if ($json->ok) {
 
-    $chatId = $json['result']['message']['from']['id'];
-    $name = $json['result']['message']['from']['first_name'];
-    $message = $json['result']['message']['text'];
+$chatId = $json['message']['chat']['id'];
+$name = $json['message']['from']['first_name'];
+$message = $json['message']['text'];
 
-    $client = new Client([
-        'base_uri' => $endpoint . $config['token'] . '/'
-    ]);
+$client = new Client([
+    'base_uri' => $endpoint . $config['token'] . '/'
+]);
 
 
-    //$response = $client->get("/sendMessage?chat_id=$chatId&text=Hola $name");
-    /*$client->get("/sendMessage?chat_id=$chatId&text=Este fue tu mensaje anterior
+//$response = $client->get("/sendMessage?chat_id=$chatId&text=Hola $name");
+/*$client->get("/sendMessage?chat_id=$chatId&text=Este fue tu mensaje anterior
     
     $message");*/
 
-    try {
-        $response = $client->request('GET', "sendMessage?chat_id=$chatId&text=Hola$name");
+try {
+    $client->request('GET', "sendMessage?chat_id=$chatId&text=Hola $name");
 
-        $log = new Logger('up');
-        $log->pushHandler(new StreamHandler('logs/debug.log', Level::Info));
+    $client->get("sendMessage?chat_id=$chatId&text=Este fue tu mensaje anterior
+    
+".json_encode($json,JSON_PRETTY_PRINT));
 
-        // add records to the log
-        $log->debug($response->getBody());
-    } catch (ClientException $e) {
-        $log = new Logger('down');
-        $log->pushHandler(new StreamHandler('logs/error.log', Level::Error));
+    $log = new Logger('up');
+    $log->pushHandler(new StreamHandler('logs/debug.log', Level::Info));
 
-        // add records to the log
-        $log->error($e->getMessage());
-    }
+    // add records to the log
+    $log->debug($response->getBody());
+} catch (ClientException $e) {
+    $log = new Logger('down');
+    $log->pushHandler(new StreamHandler('logs/error.log', Level::Error));
+
+    // add records to the log
+    $log->error($e->getMessage());
+}
 //}
