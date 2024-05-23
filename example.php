3e737416aa1bf74d81d2e034eb792555b54b6d47
alexsandrov16 <al3x5.dev@gmail.com>
2024-05-22 20:25:23 -0400

    ok

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ use Al3x5\Easybot\Api;
 require_once 'vendor/autoload.php';
 
 $bot = new Api([
-    'token' => '7066153350:AAFqaUmSIMMDRrw-WWIxctK2lw45YCyrSZU',
+    'token' => '213456:aVd_asSDLrvDDlgfZCXoyrPs',
     'commands' => []
 ]);
 //$bot->setWebhook(['url'=>'/easybot/example.php']);
