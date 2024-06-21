2213fa3f94ad60141a6c07cca94ea74d63e5f02a
alexsandrov16 <al3x5.dev@gmail.com>
2024-06-21 01:05:34 -0400

    Add entities

---
+++ src/Entities/Entity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Al3x5\Easybot\Entities;
+
+/**
+ * undocumented class
+ */
+abstract class Entity
+{
+    public function __construct(array|object $data)
+    {
+        
+    }
+}
